befcd686cfe2a6853bd86819eb662b14b9434f5f
bilalzaidi789 <syedbilalazfar@hotmail.com>
2023-07-01 22:19:51 +0500

    Create CoCart added

--- Selenium/src/FirstScript.java
+++ Selenium/src/FirstScript.java
@@ -26,23 +26,23 @@ public class FirstScript {
 		
 
 		
-        WebDriver driver1 = new ChromeDriver(options);
+        WebDriver driver1 = new ChromeDriver();
         
         //Opening google url
         
         driver1.get("https://www.coowncart.com/");
 
-      Thread.sleep(6000);
+     Thread.sleep(6000);
     
     
 
 
-   // driver.findElement(By.className("close-icon")).click();
+  //  driver1.findElement(By.className("close-icon")).click();
     
     driver1.manage().window().maximize();
 
     driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/header/div/div[2]/div/div[3]/div[2]/a/div")).click();
-    Thread.sleep(5000);
+   Thread.sleep(5000);
 
     driver1.findElement(By.xpath("//*[@id=\"panel:r0:0\"]/div/form/div[1]/div/input")).sendKeys("syed@cocarting.com");
     driver1.findElement(By.xpath("//*[@id=\"panel:r0:0\"]/div/form/div[2]/div/input")).sendKeys("123456A");
@@ -54,20 +54,37 @@ public class FirstScript {
     
 // rejecting the notification alert
     
-   // driver1.findElement(By.xpath("//*[@id=\"onesignal-slidedown-cancel-button\"]")).click();
+    driver1.findElement(By.xpath("//*[@id=\"onesignal-slidedown-cancel-button\"]")).click();
     
     
 
     
     // Go to the HomePage 
     
-    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/header/div/div[2]/div/div[1]/a")).click();
+   // driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/header/div/div[2]/div/div[1]/a")).click();
     
     Thread.sleep(10000);
 
     // Go  to create a dummy CoCart
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/main/div[4]/div/div[1]/div/div[1]/div/div[1]")).click();
+  
     
     driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[1]/div[1]/div[2]/a[1]/span[1]/img")).click();
+    
+    
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[4]/div/div/form/div[1]/div/input")).sendKeys("AUtomated CoCart");
+
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[4]/div/div/form/div[2]/div/select")).sendKeys("Private");
+  
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[4]/div/div/form/div[3]/div/textarea")).sendKeys("This CoCart is created by automated scripts");
+   
+    
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[4]/div/div/form/div[4]/div/span/input")).sendKeys("syed@cocarting.com");
+    
+    
+    driver1.findElement(By.xpath("//*[@id=\"page_wrapper\"]/div[2]/div[4]/div/div/form/div[9]/div/button")).click();
+    
+  
     
     //getting the title 
     
@@ -75,7 +92,7 @@ public class FirstScript {
        System.out.println(driver1.getCurrentUrl());
        System.out.println("Test has been passed");
        
-       Thread.sleep(5000);
+      // Thread.sleep(5000);
 
        
        driver1.close();
